38227b593bf242ff947dcc04f14bcf4472f79fbb
ArbajPathan <arbajpatel0432>
2023-12-01 20:27:51 +0530

    done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,20 +2,32 @@ import { useContext } from "react";
 import Modal from "./Modal";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
-import Card from "../../UI/Card/card";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
-  const totalAmount = cartCtx.totalAmount.toFixed(2);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItemsInCart = cartCtx.items.length > 0;
+  const addItemHandler = (item) => {
+    cartCtx.addItem({
+      ...item,
+      amount:1
+    })
+  };
+  const removeItemHandler = (id) => {
+    cartCtx.removeItem(id)
+  };
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <Card className={classes["cart-List"]}>
-          <li>
-            Name : {item.name} Price : ${item.price} Quantity : {item.amount}
-          </li>
-        </Card>
+        <CartItem
+        key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onAdd={addItemHandler.bind(null,item)}
+          onRemove={removeItemHandler.bind(null,item.id)}
+        ></CartItem>
       ))}
     </ul>
   );
@@ -25,7 +37,7 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>${totalAmount}</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onCloseCart}>
--- src/components/Layout/headerCartButton.js
+++ src/components/Layout/headerCartButton.js
@@ -1,21 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./headerCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnHighlight,setBtnHighlight]=useState(false)
   const cartCtx = useContext(CartContext);
-  console.log ('inside headerbtn' , cartCtx.items)
-  const quantity=cartCtx.items.reduce((curQuantity ,item) =>  {
+  const {items}=cartCtx
+  const quantity=items.reduce((curQuantity ,item) =>  {
     return curQuantity +Number(item.amount)
   },0)
-  // let quantity = 0;
-  // cartCtx.items.forEach((item) => {
-  //   quantity = quantity + Number(item.amount);
-  //   console.log(item);
-  // });
+  const btnClasses=`${classes.button} ${btnHighlight ?classes.bump:''}`
+  useEffect(()=> {
+    if (items.length===0){
+      return;
+    }
+    setBtnHighlight(true)
+    const timer=setTimeout(() => {
+      setBtnHighlight(false)
+    }, 300);
+    return () => {
+      clearTimeout(timer)
+    }
+  },[items])
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
+    <button className={btnClasses} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
